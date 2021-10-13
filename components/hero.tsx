f7acc35b96990083ae4e9a561ee6de4ce286bb87
Jishnu Raj <71690306+Jishnu-Dev@users.noreply.github.com>
2021-10-13 22:26:01 +0530

    Test commit

--- components/hero.tsx
+++ components/hero.tsx
@@ -32,10 +32,10 @@ export default function Hero() {
       >
         {SITE_DESCRIPTION}
       </h2>
-      <h1 className={cn(styleUtils.appear, styleUtils['appear-third'], styles.hero)}>
-        The first {BRAND_NAME}
-        <br className={styleUtils['show-on-desktop']} /> global user conference
-      </h1>
+      <h2 className={cn(styleUtils.appear, styleUtils['appear-third'], styles.hero)}>
+        Hi, I'm {BRAND_NAME}.
+        <br className={styleUtils['show-on-desktop']} />I'm A Frontend Developer
+      </h2>
       <h2
         className={cn(
           styleUtils.appear,
